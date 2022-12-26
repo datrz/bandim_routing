ddf803210cf9eaeddcaa09df18ef27793fa457b5
Damian <100217112+datrz@users.noreply.github.com>
2022-12-26 12:49:44 +0100

    Update app.py

--- app.py
+++ app.py
@@ -14,40 +14,6 @@ import datetime
 from dotenv import load_dotenv
 import os
 
-
-# concatenate the six oauth parameters, plus the request parameters from above, sorted alphabetically by the key and separated by "&"
-def create_parameter_string(grant_type, oauth_consumer_key,oauth_nonce,oauth_signature_method,oauth_timestamp,oauth_version):
-    parameter_string = ''
-    parameter_string = parameter_string + 'grant_type=' + grant_type
-    parameter_string = parameter_string + '&oauth_consumer_key=' + oauth_consumer_key
-    parameter_string = parameter_string + '&oauth_nonce=' + oauth_nonce
-    parameter_string = parameter_string + '&oauth_signature_method=' + oauth_signature_method
-    parameter_string = parameter_string + '&oauth_timestamp=' + oauth_timestamp
-    parameter_string = parameter_string + '&oauth_version=' + oauth_version
-    return parameter_string
-
-parameter_string = create_parameter_string(grant_type, oauth_consumer_key,oauth_nonce,oauth_signature_method,oauth_timestamp,oauth_version)
-encoded_parameter_string = urllib.parse.quote(parameter_string, safe='')
-encoded_base_string = 'POST' + '&' + urllib.parse.quote(url, safe='')
-encoded_base_string = encoded_base_string + '&' + encoded_parameter_string
-
-# create the signing key
-signing_key = access_key_secret + '&'
-
-oauth_signature = create_signature(signing_key, encoded_base_string)
-encoded_oauth_signature = urllib.parse.quote(oauth_signature, safe='')
-
-#---------------------Requesting Token---------------------
-body = {'grant_type' : '{}'.format(grant_type)}
-
-headers = {
-            'Content-Type' : 'application/x-www-form-urlencoded',
-            'Authorization' : 'OAuth oauth_consumer_key="{0}",oauth_nonce="{1}",oauth_signature="{2}",oauth_signature_method="HMAC-SHA256",oauth_timestamp="{3}",oauth_version="1.0"'.format(oauth_consumer_key,oauth_nonce,encoded_oauth_signature,oauth_timestamp)
-          }
-
-response = requests.post(url, data=body, headers=headers)
-bearer_token = json.loads(response.text)['access_token']
-
 #---------------------Fleet config ---------------------#
 
 d = st.sidebar.date_input("Plan", datetime.date.today())
